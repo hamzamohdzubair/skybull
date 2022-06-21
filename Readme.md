552df501df87bf17fbd433e06b0c14c6f1bb36ff
Hamza Zubair <hamzamohdzubair@github.com>
2022-06-21 09:35:34 +0530

    complete video code

---
+++ Readme.md
@@ -0,0 +1,3 @@
+A multi-threaded port scanner written in rust
+
+credit: https://www.youtube.com/watch?v=-Jp7sabBCp4&list=PLJbE2Yu2zumDD5vy2BuSHvFZU0a6RDmgb
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-use std::error::Error;
+use core::num;
+use std::{error::Error, net::{IpAddr, TcpStream}, sync::mpsc::Sender, io::{self, Write}};
 // const MAX: u16 = 65535;
 
 
 type SBResult<T> = Result<T, Box<dyn Error>>;
 
+const MAX: u16 = 65535;
+
 
 #[derive(Debug)]
 pub struct CLI {
     ip: std::net::IpAddr,
-    jobs: u8,
+    jobs: u16,
 
 }
 
@@ -39,15 +42,48 @@ pub fn get_args() -> SBResult<CLI> {
 
 }
 
+pub fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)){
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+
+        }
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+
+}
+
 pub fn run(cli: CLI) -> SBResult<()> {
+    // let addr = cli.ip;
+    // let num_threads = cli.jobs;
     let (tx, rx) = std::sync::mpsc::channel();
     for i in 0..cli.jobs {
         let tx = tx.clone();
 
         std::thread::spawn(move || {
             scan(tx, i, cli.ip, cli.jobs)
-        })
+        });
 
+    }
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
     }
 
 
